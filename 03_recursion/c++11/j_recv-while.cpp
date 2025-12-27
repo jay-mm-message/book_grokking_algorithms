0920de599330cd842d8204873fc88e45f61f2db2
jay-mm-message <jay.mm.message@gmail.com>
2025-12-27 10:46:24 +0800

    add recursive to while

---
+++ 03_recursion/c++11/j_recv-while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void count_down(int v) {
+	while(v >= 0) {
+		cout << v << ' ';
+		--v;
+	}
+	// if (v >= 0) {
+	// 	cout << v << ' ';
+	// 	return count_down(--v);
+	// }
+	// return;
+}
+
+int main(void) {
+	count_down(5);
+	cout << endl;
+	return 0;
+}
